9e569081834c3997ed776dbe17d4b92140c0000a
Sara-0215 <cruzssar@students.zhaw.ch>
2025-04-27 22:46:13 +0200

    Error correction

--- Rezeptapp/pages/Mein Konto.py
+++ Rezeptapp/pages/Mein Konto.py
@@ -1,21 +1,21 @@
 import streamlit as st
 
 def kontopage():
-st.title("👤 Mein Konto")
+    st.title("👤 Mein Konto")
 
-st.markdown("### 🥗 Diätpräferenzen festlegen")
-diät = st.radio(
-    "Meine Diät:",
-    ["Keine Einschränkung", "Vegetarisch", "Vegan", "Kein Schweinefleisch", "Pescitarisch", "laktosefrei"],
-    key="diät"
-)
+    st.markdown("### 🥗 Diätpräferenzen festlegen")
+    diät = st.radio(
+        "Meine Diät:",
+        ["Keine Einschränkung", "Vegetarisch", "Vegan", "Kein Schweinefleisch", "Pescitarisch", "laktosefrei"],
+        key="diät"
+    )
 
 
-st.markdown("### 📖 Meine Rezepte")
-for i in range(2):
-    with st.container():
-        st.image("https://source.unsplash.com/600x400/?food", width=300)
-        st.write(f"Eigenes Rezept: 🍝🍜🥗")
-        st.button("🗑️ Löschen", key=f"my_recipe_{i}")
+    st.markdown("### 📖 Meine Rezepte")
+    for i in range(2):
+        with st.container():
+            st.image("https://source.unsplash.com/600x400/?food", width=300)
+            st.write(f"Eigenes Rezept: 🍝🍜🥗")
+            st.button("🗑️ Löschen", key=f"my_recipe_{i}")
 
 
