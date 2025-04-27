984e0dad4abf7359f3c3791e5590dcfdd3c92930
Albina <krasnal2@students.zhaw.ch>
2025-04-27 23:41:41 +0200

    code nochmals angepasst

--- Rezeptapp/pages/Mein Konto.py
+++ Rezeptapp/pages/Mein Konto.py
@@ -1,21 +1,23 @@
 import streamlit as st
 
 def kontopage():
-     st.title("👤 Mein Konto")
+    # Titel ganz oben
+    st.title("📖 Mein Konto")
 
-st.markdown("### 🥗 Diätpräferenzen festlegen")
-diät = st.radio(
-    "Meine Diät:",
-    ["Keine Einschränkung", "Vegetarisch", "Vegan", "Kein Schweinefleisch", "Pescitarisch", "laktosefrei"],
-    key="diät"
-)
+    # Diätpräferenzen
+    st.markdown("### 🥗 Diätpräferenzen festlegen")
+    diät = st.radio(
+        "Meine Diät:",
+        ["Keine Einschränkung", "Vegetarisch", "Vegan", "Kein Schweinefleisch", "Pescitarisch", "laktosefrei"],
+        key="diät"
+    )
 
-st.markdown("### 📖 Meine Rezepte")
-for i in range(2):
-    with st.container():
-        st.image("https://source.unsplash.com/600x400/?food", width=300)
-        st.write(f"Eigenes Rezept: 🍝🍜🥗")
-        st.button("🗑️ Löschen", key=f"my_recipe_{i}")
+    # Eigene Rezepte anzeigen
+    st.markdown("### 📚 Meine Rezepte")
+    for i in range(2):
+        with st.container():
+            st.image("https://source.unsplash.com/600x400/?food", width=300)
+            st.write(f"Eigenes Rezept: 🍜🍹🥗")
+            st.button("🗑️ Löschen", key=f"my_recipe_{i}")
 
 kontopage()
-
