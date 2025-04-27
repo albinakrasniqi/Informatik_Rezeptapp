a1dd92ca20a18e77a6ccef2e516895e70a04ffe6
Albina <krasnal2@students.zhaw.ch>
2025-04-27 22:39:15 +0200

    korrektur

--- Rezeptapp/pages/Favoriten.py
+++ Rezeptapp/pages/Favoriten.py
@@ -1,22 +1,17 @@
 import streamlit as st
 
-#Textgrösse anpassung
-
-def text(fav):
-    st.markdown(f"<p style='font-size:{st.session_state.textgröße}px'>{text}</p>", unsafe_allow_html=True)
-
 def fav():
     st.title("❤️ Meine Favoriten")
 
-    # 🔃 Sortieroptionen
+    # 🔵 Sortieroptionen
     sort_option = st.selectbox("Sortieren nach", ["Diät", "Mahlzeit", "Zuletzt hinzugefügt", "Alt -> Neu"])
 
-    # 📋 Beispiel-Favoritenliste
+    # 🟠 Beispiel-Favoritenliste
     for i in range(2):  # Platzhalter
         with st.container():
             st.image("https://source.unsplash.com/600x400/?dish", width=300)
             st.write("**Favoritenrezept**")
-            st.write("Tags: 🥦 Vegan | Mittagessen")
+            st.write("Tags: 🍃 Vegan | 🍽️ Mittagessen")
             st.button("🗑️ Entfernen", key=f"remove_{i}")
 
 fav()
