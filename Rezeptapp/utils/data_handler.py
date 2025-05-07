57efbc1f6dedc5f0beba5b13c77a8398aa13e29a
Albina <krasnal2@students.zhaw.ch>
2025-05-07 21:17:43 +0200

    korrektur

--- Rezeptapp/utils/data_handler.py
+++ Rezeptapp/utils/data_handler.py
@@ -42,7 +42,7 @@ class DataHandler:
         full_path = self._resolve_path(relative_path)
         return self.filesystem.exists(full_path)
 
-    def read_text(self, relative_path):
+    def read_text(self, relative_path, encoding= "utf-8"):
         """
         Read the contents of a text file.
 
@@ -53,7 +53,7 @@ class DataHandler:
             The content of the file as a string.
         """
         full_path = self._resolve_path(relative_path)
-        with self.filesystem.open(full_path, "r") as f:
+        with self.filesystem.open(full_path, "r", encoding=encoding) as f:
             return f.read()
 
     def read_binary(self, relative_path):
