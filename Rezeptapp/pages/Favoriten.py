4c1568ae9707be44f9f610e4acc646731a239db9
Gen-ta <arifigen@students.zhaw.ch>
2025-05-14 17:32:19 +0200

    Änderungen

--- Rezeptapp/pages/Favoriten.py
+++ Rezeptapp/pages/Favoriten.py
@@ -3,22 +3,6 @@ import streamlit as st
 def fav():
     st.title("❤️ Meine Favoriten")
 
-    # 🔵 Sortieroptionen
-    sort_option = st.selectbox("Sortieren nach", ["Diät", "Mahlzeit", "Zuletzt hinzugefügt", "Alt -> Neu"])
-
-    # 🟠 Beispiel-Favoritenliste
-    for i in range(2):  # Platzhalter
-        with st.container():
-            st.image("https://source.unsplash.com/600x400/?dish", width=300)
-            st.write("**Favoritenrezept**")
-            st.write("Tags: 🍃 Vegan | 🍽️ Mittagessen")
-            st.button("🗑️ Entfernen", key=f"remove_{i}")
-
-fav()
-
-def fav():
-    st.title("❤️ Meine Favoriten")
-
     sort_option = st.selectbox("Sortieren nach", ["Diät", "Mahlzeit", "Zuletzt hinzugefügt", "Alt -> Neu"])
     sortieren = st.button("🔃 Sortierung anwenden")
 
